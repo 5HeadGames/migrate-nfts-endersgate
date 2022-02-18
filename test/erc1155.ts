1a60e4e7281a41d4a6b31f407926dcecca36750c
rafaelChelo <rafael@chelofinance.io>
2022-02-17 21:05:20 -0400

    Test suite template implemented

---
+++ test/erc1155.ts
@@ -0,0 +1,16 @@
+import {ethers, upgrades, network} from "hardhat";
+import {BigNumber, BigNumberish} from "@ethersproject/bignumber";
+import {expect, assert} from "chai";
+import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+
+
+describe("ERC1155", function () {
+  before(async function () {
+  });
+
+  it("Should create portfolio manager", async () => {
+  });
+
+});
+
+
--- test/exchange.ts
+++ test/exchange.ts
@@ -1,15 +1,29 @@
-import {ethers, upgrades, network, web3} from "hardhat";
-import {BigNumber, BigNumberish} from "@ethersproject/bignumber";
-import {expect, assert} from "chai";
+import {ethers, upgrades, network} from "hardhat";
+import {expect} from "chai";
 import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 
+import {ExchangeERC1155} from "../types/ExchangeERC1155";
+import {EndersGate} from "../types/EndersGate";
+import {ERC1155card} from "../types/ERC1155card";
 
-describe.only("Chelofinance", function () {
-  before(async function () {
+describe("Exchange", function () {
+  let exchange: ExchangeERC1155,
+    newNft: EndersGate,
+    oldNft: ERC1155card,
+    oldNft2: ERC1155card,
+    accounts: SignerWithAddress[];
+
+  it('Should deploy properly', async () => {
+    newNft = (await upgrades.deployProxy(await ethers.getContractFactory("EndersGate"), {
+      kind: "uups",
+    })) as EndersGate;
+    oldNft = await (await ethers.getContractFactory("ERC1155card")).deploy("Dracul");
+    oldNft2 = await (await ethers.getContractFactory("ERC1155card")).deploy("Eross");
+    exchange = await (
+      await ethers.getContractFactory("ExchangeERC1155")
+    ).deploy([oldNft.address, oldNft2.address], [1, 1], [1, 2]);
+    expect(true, "Deploy failed")
   });
 
-  it("Should create portfolio manager", async () => {
-  });
-
+  it("Should create portfolio manager", async () => {});
 });
-
