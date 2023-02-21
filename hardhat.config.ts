da4f7f7a527e3f0642277d4b026861751a23ea3e
Andres Contreras <andrescontrerasoviedo740@gmail.com>
2023-02-21 07:48:09 -0400

    added marketplaces, scripts URIs from HVO and AVS

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -92,6 +92,42 @@ const config: HardhatUserConfig = {
       {
         version: "0.8.0",
       },
+      {
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+      {
+        version: "0.8.11",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+      {
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+      {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
     ],
     settings: {
       metadata: {
--- utils/index.ts
+++ utils/index.ts
@@ -1,111 +1,136 @@
 import Moralis from "moralis/node";
 import fs from "fs";
 import * as dotenv from "dotenv";
-import {TransactionReceipt} from "@ethersproject/providers";
+import { TransactionReceipt } from "@ethersproject/providers";
+
+import { ContractReceipt } from "@ethersproject/contracts";
+import { Interface, LogDescription } from "@ethersproject/abi";
+
+export const getLogs = (iface: Interface, transaction: ContractReceipt) => {
+  const response: LogDescription[] = [];
+  transaction.logs.forEach((log) => {
+    try {
+      response.push(iface.parseLog(log));
+    } catch (err: any) {}
+  });
+  return response;
+};
 
 dotenv.config();
 const appRoot = require("app-root-path");
 
-export const uploadIpfs = async ({path}: {path: string}) => {
-    try {
-        const name = path
-            .split("/")
-            .reverse()[0]
-            .replace(".", "")
-            .replace("'", "")
-            .replace(":", "")
-            .replace("&", "")
-            .replace("_", "")
-            .replace("-", "");
+export const uploadIpfs = async ({ path }: { path: string }) => {
+  try {
+    const name = path
+      .split("/")
+      .reverse()[0]
+      .replace(".", "")
+      .replace("'", "")
+      .replace(":", "")
+      .replace("&", "")
+      .replace("_", "")
+      .replace("-", "");
 
-        await Moralis.start({
-            serverUrl: process.env.MORALIS_SERVER_URL,
-            appId: process.env.MORALIS_APP_ID,
-            masterKey: process.env.MORALIS_MASTER_KEY,
-        });
+    await Moralis.start({
+      serverUrl: process.env.MORALIS_SERVER_URL,
+      appId: process.env.MORALIS_APP_ID,
+      masterKey: process.env.MORALIS_MASTER_KEY,
+    });
 
-        const buff = fs.readFileSync(`${appRoot}${path}`).toString("base64");
-        const file = new Moralis.File(name, {base64: buff});
+    const buff = fs.readFileSync(`${appRoot}${path}`).toString("base64");
+    const file = new Moralis.File(name, { base64: buff });
 
-        console.log("UPLOADING FILE", `${appRoot}${path}`);
-        await file.saveIPFS({useMasterKey: true});
-        const hash = (file as any).ipfs() as string;
+    console.log("UPLOADING FILE", `${appRoot}${path}`);
+    await file.saveIPFS({ useMasterKey: true });
+    const hash = (file as any).ipfs() as string;
 
-        console.log("uploaded", hash);
-        return hash;
-    } catch (err: any) {
-        console.log(`ERROR upload ${path}`, err.message);
-    }
-    return "";
+    console.log("uploaded", hash);
+    return hash;
+  } catch (err: any) {
+    console.log(`ERROR upload ${path}`, err.message);
+  }
+  return "";
 };
 
-export const uploadIpfsObject = async ({element, name}: {element: Object; name: string}) => {
-    try {
-        const finalName = name
-            .replace(".", "")
-            .replace("'", "")
-            .replace(":", "")
-            .replace("&", "")
-            .replace("_", "")
-            .replace("-", "");
+export const uploadIpfsObject = async ({
+  element,
+  name,
+}: {
+  element: Object;
+  name: string;
+}) => {
+  try {
+    const finalName = name
+      .replace(".", "")
+      .replace("'", "")
+      .replace(":", "")
+      .replace("&", "")
+      .replace("_", "")
+      .replace("-", "");
 
-        await Moralis.start({
-            serverUrl: process.env.MORALIS_SERVER_URL,
-            appId: process.env.MORALIS_APP_ID,
-            masterKey: process.env.MORALIS_MASTER_KEY,
-        });
+    await Moralis.start({
+      serverUrl: process.env.MORALIS_SERVER_URL,
+      appId: process.env.MORALIS_APP_ID,
+      masterKey: process.env.MORALIS_MASTER_KEY,
+    });
 
-        const buff = Buffer.from(JSON.stringify(element)).toString("base64");
-        const file = new Moralis.File(finalName, {base64: buff});
+    const buff = Buffer.from(JSON.stringify(element)).toString("base64");
+    const file = new Moralis.File(finalName, { base64: buff });
 
-        console.log("UPLOADING...", name);
-        await file.saveIPFS({useMasterKey: true});
+    console.log("UPLOADING...", name);
+    await file.saveIPFS({ useMasterKey: true });
 
-        return (file as any).ipfs() as string;
-    } catch (err: any) {
-        console.log(`ERROR upload ${name}`, err.message);
-    }
-    return "";
+    return (file as any).ipfs() as string;
+  } catch (err: any) {
+    console.log(`ERROR upload ${name}`, err.message);
+  }
+  return "";
 };
 
 export const loadJsonFile = (file: string) => {
-    const appRoot = require("app-root-path");
-    try {
-        const data = fs.readFileSync(`${appRoot}${file[0] === "/" ? file : "/" + file}`);
-        return JSON.parse(data as any);
-    } catch (err) {
-        return {};
-    }
+  const appRoot = require("app-root-path");
+  try {
+    const data = fs.readFileSync(
+      `${appRoot}${file[0] === "/" ? file : "/" + file}`,
+    );
+    return JSON.parse(data as any);
+  } catch (err) {
+    return {};
+  }
 };
 
-export const writeJsonFile = (args: {path: string; data: Object | ((arg: Object) => void)}) => {
-    const appRoot = require("app-root-path");
-    const prevData = loadJsonFile(args.path);
-    const parsedData = JSON.stringify(
-        typeof args.data === "function"
-            ? {
-                ...args.data(prevData),
-            }
-            : {
-                ...prevData,
-                ...args.data,
-            },
-        null,
-        2
-    );
-    console.log("Writting", appRoot + args.path);
-    fs.writeFileSync(appRoot + args.path, parsedData);
-    console.log(`Generated ${appRoot}${args.path}`);
+export const writeJsonFile = (args: {
+  path: string;
+  data: Object | ((arg: Object) => void);
+}) => {
+  const appRoot = require("app-root-path");
+  const prevData = loadJsonFile(args.path);
+  const parsedData = JSON.stringify(
+    typeof args.data === "function"
+      ? {
+          ...args.data(prevData),
+        }
+      : {
+          ...prevData,
+          ...args.data,
+        },
+    null,
+    2,
+  );
+  console.log("Writting", appRoot + args.path);
+  fs.writeFileSync(appRoot + args.path, parsedData);
+  console.log(`Generated ${appRoot}${args.path}`);
 };
 
 export const getEventLogs = (
-    iface: any,
-    logs: TransactionReceipt["logs"],
-    filter: (log: any) => boolean
+  iface: any,
+  logs: TransactionReceipt["logs"],
+  filter: (log: any) => boolean,
 ) => {
-    return logs.filter(filter).map((logs) => iface.parseLog(logs));
+  return logs.filter(filter).map((logs) => iface.parseLog(logs));
 };
 
 export const getUrlHash = (ipfsUrl: string) => ipfsUrl.split("/").reverse()[0];
 
-export const wait = (seconds: number) => new Promise((res) => setTimeout(res, seconds));
+export const wait = (seconds: number) =>
+  new Promise((res) => setTimeout(res, seconds));
